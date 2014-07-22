1c91624604648deb3938d7dc85884faf963062b8
Peter Jaros <peter.a.jaros@gmail.com>
2014-07-22 11:50:20 -0400

    Don't use alias_method_chain.

--- lib/octoshark/active_record_extensions.rb
+++ lib/octoshark/active_record_extensions.rb
@@ -2,15 +2,9 @@ module Octoshark
   module ActiveRecordBase
     extend ActiveSupport::Concern
 
-    included do
-      class << self
-        alias_method_chain :establish_connection, :octoshark
-      end
-    end
-
     module ClassMethods
-      def establish_connection_with_octoshark(*args)
-        establish_connection_without_octoshark(*args)
+      def establish_connection(*)
+        super
         Octoshark.reload! if Octoshark.configured?
       end
     end
@@ -19,16 +13,12 @@ module Octoshark
   module ActiveRecordLogSubscriber
     extend ActiveSupport::Concern
 
-    included do
-      alias_method_chain :debug, :octoshark
-    end
-
-    def debug_with_octoshark(msg)
+    def debug(msg)
       prefix = if Octoshark.configured? && Octoshark.current_connection_name
                  color("[Octoshark: #{Octoshark.current_connection_name}]",
                        ActiveSupport::LogSubscriber::GREEN, true)
                end
-      debug_without_octoshark("#{prefix}#{msg}")
+      super("#{prefix}#{msg}")
     end
   end
 end
