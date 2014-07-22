2cfe1a2d649b9696081f7b661254ebe199a0c360
Peter Jaros <peter.a.jaros@gmail.com>
2014-07-22 15:55:49 -0400

    Log connection name for Octoshark connection only.

--- spec/octoshark/active_record_extensions_spec.rb
+++ spec/octoshark/active_record_extensions_spec.rb
@@ -26,4 +26,20 @@ describe "ActiveRecord Extensions" do
 
     ActiveRecord::Base.logger = nil
   end
+
+  it "logs the connection name for the Octoshark connection only" do
+    io = StringIO.new
+    logger = Logger.new(io)
+
+    ActiveRecord::Base.logger = logger
+
+    Octoshark.configure(configs)
+    Octoshark.with_connection(:db1) do |connection|
+      ActiveRecord::Base.connection.execute("SELECT 1")
+    end
+
+    expect(io.string).not_to include('[Octoshark: db1]')
+
+    ActiveRecord::Base.logger = nil
+  end
 end
