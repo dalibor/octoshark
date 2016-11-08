b764732149a9f686ee7511997cd6f331a887278f
Dalibor Nasevic <dalibor.nasevic@gmail.com>
2016-11-08 22:23:54 +0100

    Fix undefined method `connection' when using Passenger server

--- lib/octoshark/active_record_extensions.rb
+++ lib/octoshark/active_record_extensions.rb
@@ -1,8 +1,8 @@
 module Octoshark
   module ConnectionHandler
     def establish_connection(*args)
-      super(*args)
       Octoshark::ConnectionPoolsManager.reset_connection_managers!
+      super(*args)
     end
   end
 
@@ -19,10 +19,9 @@ module Octoshark
   end
 end
 
-if defined?(ActiveRecord::ConnectionAdapters::ConnectionSpecification)
+if defined?(ActiveRecord::ConnectionAdapters::ConnectionHandler)
+  # Rails 3.2, 4.0, 4.1, 4.2, 5.0
   ActiveRecord::ConnectionAdapters::ConnectionHandler.send(:prepend, Octoshark::ConnectionHandler)
-elsif defined?(ActiveRecord::ConnectionAdapters::ConnectionPool::ConnectionHandler)
-  ActiveRecord::ConnectionAdapters::ConnectionPool::ConnectionHandler.send(:prepend, Octoshark::ConnectionHandler)
 else
   # Rails 3.0 and 3.1 does not lazy load
   require 'active_record/connection_adapters/abstract_adapter'
